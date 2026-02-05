3978253d231e2131cc88a6a3e5e6714d44eca6a2
Erasyl <erbulekerasyl@gmail.com>
2026-02-05 16:03:14 +0600

    prac02 in progress

---
+++ prac02/if_else/if_elif_else.py
@@ -0,0 +1,21 @@
+# if + elif + else
+score = 82
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 75:
+    print("Grade: B")
+elif score >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+# ещё пример
+age = 15
+
+if age < 13:
+    print("Child")
+elif age < 18:
+    print("Teenager")
+else:
+    print("Adult")
